51c39331cadaf308cae94416c0ca3bc18598ddd5
tguyonnet <guyonn28th@lycee-ndduroc.com>
2018-11-25 17:00:12 +0100

    update indexFaker

--- indexFaker.php
+++ indexFaker.php
@@ -6,3 +6,37 @@
  * Time: 16:54
  */
 
+require './vendor/autoload.php';
+
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \RedBeanPHP\R as R;
+
+
+//R::setup('mysql:host='.\Config\Bdd::HOST.';dbname='.\Config\Bdd::DBNAME.'',\Config\Bdd::USERNAME,\Config\Bdd::PASSWORD);
+R::setup('mysql:host=localhost;dbname=sanofi_v0','usersio','pwsio');
+
+
+$dataGenerator = Faker\Factory::create('fr_FR');
+
+
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+
+$c = new \Slim\Container($configuration);
+$app = new \Slim\App($c);
+
+
+$app->get('/', function ($request, $response, $args) {
+
+
+
+
+});
+
+$app->run();
+
